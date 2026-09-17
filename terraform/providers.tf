c7625c638c37fe6e11ab7eca4690a09732b41d47
William <w.slooten7@upcmail.nl>
2026-09-17 14:37:35 +0200

    terraform: configure Azure remote state backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,6 +4,14 @@ terraform {
       source = "hashicorp/azurerm"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state-swc-001"
+    storage_account_name = "wslootentfstate2026"
+    container_name       = "tfstate"
+    key                  = "enterprise-platform.tfstate"
+    use_azuread_auth     = true
+  }
 }
 
 provider "azurerm" {
